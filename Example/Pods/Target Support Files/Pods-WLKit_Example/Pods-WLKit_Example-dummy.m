9ccb18ffaeeccf28791f778e438142056b7e827b
巫龙 <454763196@qq.com>
2016-01-13 22:44:37 +0800

    # change files

--- Example/Pods/Target Support Files/Pods-WLKit_Example/Pods-WLKit_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WLKit_Example : NSObject
-@end
-@implementation PodsDummy_Pods_WLKit_Example
-@end
--- Example/Pods/Target Support Files/WLKit/WLKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_WLKit : NSObject
-@end
-@implementation PodsDummy_WLKit
-@end
